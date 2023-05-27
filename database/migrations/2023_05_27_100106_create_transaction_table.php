2b68e638493fd79dee9e33241d2dd3c1a6c012f1
Błażej Homa <blazejh@op.pl>
2023-05-27 12:03:43 +0200

    migrations: doctors, surgery, transaction

---
+++ database/migrations/2023_05_27_100106_create_transaction_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('transaction', function (Blueprint $table) {
+            $table->id();
+            $table->date('registration');
+            $table->integer('price')->unique();
+            $table->integer('treatments_performed')->unique();
+            $table->id('surgery_id')->unique();
+            $table->id('doctors_id')->unique();
+            $table->id('users_id')->unique();
+            $table->rememberToken();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('transaction');
+    }
+};
