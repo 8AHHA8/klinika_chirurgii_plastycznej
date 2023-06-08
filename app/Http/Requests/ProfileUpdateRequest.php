7ff4fb9bd19ef014da1aae8080e034ecf685c43b
Błażej Homa <blazejh@op.pl>
2023-06-08 23:45:00 +0200

    self editing for users has been perfected

--- app/Http/Requests/ProfileUpdateRequest.php
+++ app/Http/Requests/ProfileUpdateRequest.php
@@ -8,15 +8,15 @@ use Illuminate\Validation\Rule;
 
 class ProfileUpdateRequest extends FormRequest
 {
-    /**
-     * Get the validation rules that apply to the request.
-     *
-     * @return array<string, \Illuminate\Contracts\Validation\Rule|array|string>
-     */
+    
     public function rules(): array
     {
         return [
             'name' => ['string', 'max:255'],
+            'surname' => ['string', 'max:255'],
+            'phone_number' => ['required', 'string', 'size:9'],
+            'pesel' => ['required', 'string', 'size:11'],
+
             'email' => ['email', 'max:255', Rule::unique(User::class)->ignore($this->user()->id)],
         ];
     }
